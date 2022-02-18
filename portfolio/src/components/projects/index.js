f662822de5d5d8c8426c71f165c23fcc5f1f578c
Omarjcp <omar.castillo1095@gmail.com>
2022-02-18 18:59:47 -0300

    feat component project

--- portfolio/src/components/projects/index.js
+++ portfolio/src/components/projects/index.js
@@ -1,9 +1,35 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Text,
+  Image,
+  Collapse,
+  Button,
+  useDisclosure,
+  Link,
+} from "@chakra-ui/react";
+
+import ImageDogPage from "./assets/imageDogPag.png";
+
+import "./style.scss";
 
 export const Projects = () => {
+  const { isOpen, onToggle } = useDisclosure();
   return (
-    <Box>
-      <Heading>PROYECTOS</Heading>
+    <Box className="container-main-projects">
+      <Box className="container-first-project">
+        <Link href="https://mydogs-b0f97.web.app/" target="_blank">
+          <Image
+            w="20rem"
+            alt="image landing page my dog app"
+            src={ImageDogPage}
+            onClick={onToggle}
+          />
+        </Link>
+        <Box color="white" bg="black" rounded="md" shadow="md">
+          lorem ipsu
+        </Box>
+      </Box>
     </Box>
   );
 };
